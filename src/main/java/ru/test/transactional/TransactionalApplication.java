276b4a4ef5aa72f2596adb87e79b74c5c3634565
Кирилл Селиванов <kselivanov@diasofft.ru>
2024-04-12 10:36:48 +0300

    1

--- src/main/java/ru/test/transactional/TransactionalApplication.java
+++ src/main/java/ru/test/transactional/TransactionalApplication.java
@@ -1,18 +1,8 @@
 package ru.test.transactional;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
-import org.springframework.cloud.stream.binder.BinderFactory;
-import org.springframework.cloud.stream.binder.kafka.KafkaMessageChannelBinder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.transaction.ChainedKafkaTransactionManager;
-import org.springframework.kafka.transaction.KafkaTransactionManager;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.ConfigurableApplicationContext;
 import ru.test.transactional.service.PersonService;
 
 import java.util.UUID;
@@ -21,38 +11,9 @@ import java.util.UUID;
 public class TransactionalApplication {
 
     public static void main(String[] args) throws InterruptedException {
-        var context = SpringApplication.run(TransactionalApplication.class, args);
-        var service = context.getBean(PersonService.class);
-        service.save("Name " + UUID.randomUUID());
-    }
-    @Bean
-    KafkaTransactionManager customKafkaTransactionManager(BinderFactory binderFactory) {
-        KafkaMessageChannelBinder kafka = (KafkaMessageChannelBinder) binderFactory.getBinder("kafka", MessageChannel.class);
-        ProducerFactory<byte[], byte[]> transactionalProducerFactory = kafka.getTransactionalProducerFactory();
-        KafkaTransactionManager kafkaTransactionManager = new KafkaTransactionManager(transactionalProducerFactory);
-        return kafkaTransactionManager;
-    }
-    @Bean
-    public PlatformTransactionManager transactionManager(ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
-        return transactionManager;
-    }
-
-    @Bean
-    public ChainedKafkaTransactionManager chainedKafkaTransactionManager(KafkaTransactionManager kafkaTransactionManager, PlatformTransactionManager transactionManager) {
-        return new ChainedKafkaTransactionManager(kafkaTransactionManager, transactionManager);
-    }
-    @Bean
-    public Runnable run() {
-        return () -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-
-            }
-//            throw new RuntimeException("oops...");
-        };
+        ConfigurableApplicationContext context = SpringApplication.run(TransactionalApplication.class, args);
+        PersonService service = context.getBean(PersonService.class);
+        service.save("Name 1 " + UUID.randomUUID());
     }
 
 }
--- src/main/java/ru/test/transactional/service/PersonService.java
+++ src/main/java/ru/test/transactional/service/PersonService.java
@@ -10,22 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.test.transactional.entity.Person;
 import ru.test.transactional.repo.PersonRepository;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PersonService {
     private final PersonRepository repo;
     private final StreamBridge bridge;
-    private final Runnable run;
 
-    @Transactional("transactionManager")
+
+    @Transactional
     public void save(String person) throws InterruptedException {
-        log.info("====== Save person =======");
-        repo.save(new Person(person));
         log.info("====== send person ========");
-        bridge.send("topic", person);
-        log.info("====== exception ======= ");
-        throw new RuntimeException();
+        bridge.send("test-exception", person);
+        bridge.send("test-exception", person);
+        bridge.send("test-exception", person);
+        bridge.send("test-exception", person);
+        log.info("====== end ========");
+        Thread.sleep(2000);
+//        log.info("====== Save person =======");
+//        repo.save(new Person(person));
+        throw  new RuntimeException();
     }
+    @KafkaListener(topics = "test-exception", groupId = "a")
+    public void get(String s) {
+        System.out.println(s);
+    }
+
 
 }
